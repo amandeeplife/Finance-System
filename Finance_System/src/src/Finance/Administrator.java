401a3c874840edfdf68b26f7673bba85dcca6629
amandeeplife <aman.yahway@gmail.com>
2017-12-01 21:21:27 -0500

    Organised files

---
+++ Finance_System/src/src/Finance/Administrator.java
@@ -0,0 +1,16 @@
+package src.Finance;
+
+public class Administrator extends DeptEmployee {
+ 
+	 int overtimeHours;
+	Administrator(String name, double salary, int yearOfHire, int monthOfHire, int dayOfHire,int overtimeHours) {
+		super(name, salary, yearOfHire, monthOfHire, dayOfHire);
+		 this.overtimeHours=overtimeHours;
+	
+	}
+	
+	public double computeSalary(){
+		return salary=salary +(20*overtimeHours);
+	}
+}
+
---
+++ Finance_System/src/src/Finance/Secretary.java
@@ -0,0 +1,17 @@
+package src.Finance;
+
+public class Secretary extends DeptEmployee {
+	int overtimeHours;
+	
+	Secretary(String name, double salary, int yearOfHire, int monthOfHire, int dayOfHire,int overtimeHours) {
+		super(name, salary, yearOfHire, monthOfHire, dayOfHire);
+		 this.overtimeHours=overtimeHours;
+	}
+
+
+
+	public double computeSalary(){
+		return salary=salary +(12*overtimeHours);
+	}
+
+}
